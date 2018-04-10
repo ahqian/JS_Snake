3cf5c0be62cae0aaa26f9fb0168ee177da6581a8
ahqian <alex_q@hotmail.ca>
2018-04-10 18:03:34 -0400

    game is more modular. Main.js acts as an engine, snake games now must provide loop(timestamp) and keyPress(key)

--- main.js
+++ main.js
@@ -4,150 +4,51 @@ function loadScript(url, callback) {
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
-    
+
     script.onreadystatechange = callback;
     script.onload = callback;
-    
+
     document.head.appendChild(script);
 }
 
 function keyPush(key) {
-    switch(key.keyCode) {
-        case 37:
-            if (snake.xv != 1) {
-                snake.xv = -1;
-                snake.yv = 0;
-            }
-            break;
-        case 38:
-            if (snake.yv != 1) {
-                snake.xv = 0;
-                snake.yv = -1;
-            }
-            break;
-        case 39:
-            if (snake.xv != -1) {
-                snake.xv = 1;
-                snake.yv = 0;
-            }
-            break;
-        case 40:
-            if (snake.yv != -1) {
-                snake.xv = 0;
-                snake.yv = 1;
-            }
-            break;
-        default:
-            console.log("unknown key: " + key.keyCode);
-            break;
-    }
-}
-
-//update world state for one elapse game frame
-function update() {
-    var sBody = snake.body;
-    var apples = snake.apples;
-    //new snake head position
-    var nx = sBody[0].x + (snake.xv * snake.tileWidth);
-    var ny = sBody[0].y + (snake.yv * snake.tileHeight);
-    
-    //out of bounds check
-    if (nx >= snake.width || nx < 0 || ny >= snake.height || ny < 0) {
-        
-        return;
-    }
-    
-    //apples collision check
-    var applesToAdd = 0;
-    for (var i = 0; i < apples.length; i++) {
-        if (sBody[0].x == apples[i].x
-           && sBody[0].y == apples[i].y) {
-            
-            sBody.push({x: apples[i].x, y: apples[i].y});
-            apples.splice(i, 1);
-            i--; //because we deleted an apple, the next is now in current index
-            
-            applesToAdd++;
-        }
-    }
-    
-    //add new apples
-    for (var i = 0; i < applesToAdd; i++) {
-        //create new apple in different spot
-        //todo: find spot not currently filled by snake
-        apples.push({
-                        x: Math.floor(Math.random() * (snake.xtiles - 1)) * snake.tileWidth,
-                        y: Math.floor(Math.random() * (snake.ytiles - 1)) * snake.tileHeight
-                    });
-        console.log("New apple x: " + apples[apples.length - 1].x
-                   + " y: " + apples[apples.length - 1].y);
-    }
-        
-    //move the snake forward
-    var tail = sBody.pop();
-    snake.clearx = tail.x;
-    snake.cleary = tail.y;
-    
-    //depending on underlying data structure this could suck
-    //should ensure linked list structure with tail tracking
-    sBody.unshift({x: nx, y: ny});
-}
-
-function draw() {
-    var ctx = snake.ctx;
-    var sBody = snake.body;
-    var bs = snake.borderSize;
-    var apples = snake.apples;
-        
-    //draw apples
-    for (var i = 0; i < apples.length; i++) {
-        ctx.fillStyle = "green";
-        ctx.fillRect(apples[i].x, apples[i].y, snake.tileWidth, snake.tileHeight);
-    }
-    
-    //draw snake
-    for (var i = 0; i < sBody.length; i++) {
-        ctx.fillStyle = "black";
-        
-        //draw snake body border
-        ctx.fillRect(sBody[i].x, sBody[i].y, snake.tileWidth, snake.tileHeight);
-        //clear old body
-        ctx.fillRect(snake.clearx, snake.cleary, snake.tileWidth, snake.tileHeight);
-        
-        //draw snake body
-        ctx.fillStyle = "white";
-        ctx.fillRect(sBody[i].x + bs, sBody[i].y + bs, snake.tileWidth - bs, snake.tileHeight - bs);
+    for (var i = 0; i < games.length; i++) {
+        games[i].keyPush(key);
     }
 }
 
 function loop(timestamp) {
-    
-    snake.progress += timestamp - snake.lastRender;
-    
-    
-    if (snake.progress > snake.frameTime) {
-        snake.progress = 0;
-        update();
+    for (var i = 0; i < games.length; i++) {
+        games[i].loop(timestamp);
     }
-    draw();
-    
-    snake.lastRender = timestamp;
     window.requestAnimationFrame(loop);
+}
+
+//create a Snake game canvas within the given containerElement
+//add it to the global games Array
+function addSnakeGame(containerElement) {
+    var canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "gc" + games.length);
+    containerElement.appendChild(canvas);
+    var snake = new Snake("gc" + games.length);
+    games.push(snake);
 }
 
 //calback after main snake script loaded
 function main() {
-    
+
+    addSnakeGame(document.body);
+    addSnakeGame(document.body);
+
     //register event listeners
     document.addEventListener("keydown", keyPush);
-    
-    //initialize game context
-    snake = new Snake();
-    
+
     //main loop
     window.requestAnimationFrame(loop);
 }
 
-var snake; // initialized in main after snake.js loaded
-document.onload = loadScript("snake.js", main);
+//game must have loop(timestamp) and keyPush(key) functions defined
+//game is passed in the id of its canvas
+var games = [];
 
+document.onload = loadScript("snake.js", main);
--- snake.js
+++ snake.js
@@ -1,31 +1,189 @@
+'use strict';
+
 //game context
-function Snake() {
-    
-    this.framerate = 15;
+function Snake(canvasId, gameSettings) {
+
+    this.snake = this;
+
+    this.framerate = 7;
     this.tileWidth = 25;
-    this.tileHeight = 25; 
+    this.tileHeight = 25;
     this.xtiles = 16;
     this.ytiles = 16;
     this.borderSize = 2;
-    
+
     //graphics context
-    this.gc = document.getElementById('gc');
+    this.gc = document.getElementById(canvasId);
     this.width = this.tileWidth * this.xtiles;
     this.height = this.tileHeight * this.ytiles;
     this.gc.setAttribute("width", this.width);
     this.gc.setAttribute("height", this.height);
-    this.ctx = gc.getContext('2d');
-    
+    this.ctx = this.gc.getContext('2d');
+
     //rendering/game update counters
     this.lastRender = 0;
-    this.frameTime = 1000/this.framerate;
+    this.frameTime = 1000 / this.framerate;
     this.progress = 0;
-    
+
     //game objects
-    this.body = [{x: 0, y: 0}];
+    this.body = [{
+        x: 0,
+        y: 0
+    }];
     this.xv = 0; //x velocity
     this.yv = 0; //y velocity
-    this.clearx = -this.tileWidth;
-    this.cleary = -this.tileHeight;
-    this.apples = [{x: 5*this.tileWidth, y: 0}];
+    this.apples = [{
+        x: 5 * this.tileWidth,
+        y: 0
+    }];
+
+    //game functions
+    this.reset = function () {
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0, 0, this.gc.width, this.gc.height);
+
+        this.body = [{
+            x: 0,
+            y: 0
+        }];
+        this.apples = [{
+            x: 5 * this.tileWidth,
+            y: 0
+        }];
+
+        this.xv = 0; //x velocity
+        this.yv = 0; //y velocity
+    }
+
+    //update world for one elapsed game frame
+    this.update = function () {
+        var sBody = this.body;
+        var apples = this.apples;
+        //new snake head position
+        var nx = sBody[0].x + (this.xv * this.tileWidth);
+        var ny = sBody[0].y + (this.yv * this.tileHeight);
+
+        //out of bounds check
+        if (nx >= this.width || nx < 0 || ny >= this.height || ny < 0) {
+            this.reset();
+            return;
+        }
+
+        //apples collision check
+        var applesToAdd = 0;
+        for (var i = 0; i < apples.length; i++) {
+            if (sBody[0].x == apples[i].x &&
+                sBody[0].y == apples[i].y) {
+
+                sBody.push({
+                    x: apples[i].x,
+                    y: apples[i].y
+                });
+                apples.splice(i, 1);
+                i--; //because we deleted an apple, the next is now in current index
+
+                applesToAdd++;
+            }
+        }
+
+        //add new apples
+        for (var i = 0; i < applesToAdd; i++) {
+            //create new apple in different spot
+            //todo: find spot not currently filled by snake
+            apples.push({
+                x: Math.floor(Math.random() * (this.xtiles - 1)) * this.tileWidth,
+                y: Math.floor(Math.random() * (this.ytiles - 1)) * this.tileHeight
+            });
+            //console.log("New apple x: " + apples[apples.length - 1].x + " y: " + apples[apples.length - 1].y);
+        }
+
+        //move the snake forward
+        var tail = sBody.pop();
+        this.clearx = tail.x;
+        this.cleary = tail.y;
+
+        //depending on underlying data structure this could suck
+        //should ensure linked list structure with tail tracking
+        sBody.unshift({
+            x: nx,
+            y: ny
+        });
+    }
+
+    //top level draw for whole game using given canvas
+    this.draw = function () {
+        var ctx = this.ctx;
+        var sBody = this.body;
+        var bs = this.borderSize;
+        var apples = this.apples;
+
+        //draw background
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, this.gc.width, this.gc.height);
+
+        //draw apples
+        for (var i = 0; i < apples.length; i++) {
+            ctx.fillStyle = "green";
+            ctx.fillRect(apples[i].x, apples[i].y, this.tileWidth, this.tileHeight);
+        }
+
+        //draw snake
+        for (var i = 0; i < sBody.length; i++) {
+            ctx.fillStyle = "black";
+
+            //draw snake body border
+            ctx.fillRect(sBody[i].x, sBody[i].y, this.tileWidth, this.tileHeight);
+
+            //draw snake body
+            ctx.fillStyle = "white";
+            ctx.fillRect(sBody[i].x + bs, sBody[i].y + bs, this.tileWidth - bs, this.tileHeight - bs);
+        }
+    }
+
+    //called by the outer keyDown event listener
+    this.keyPush = function (key) {
+        switch (key.keyCode) {
+            case 37:
+                if (this.xv != 1) {
+                    this.xv = -1;
+                    this.yv = 0;
+                }
+                break;
+            case 38:
+                if (this.yv != 1) {
+                    this.xv = 0;
+                    this.yv = -1;
+                }
+                break;
+            case 39:
+                if (this.xv != -1) {
+                    this.xv = 1;
+                    this.yv = 0;
+                }
+                break;
+            case 40:
+                if (this.yv != -1) {
+                    this.xv = 0;
+                    this.yv = 1;
+                }
+                break;
+            default:
+                console.log("unknown key: " + key.keyCode);
+                break;
+        }
+    }
+
+    //loop function to execute one cycle of game time
+    //called by an outer loop
+    this.loop = function (timestamp) {
+        this.progress += timestamp - this.lastRender;
+
+        if (this.progress > this.frameTime) {
+            this.progress = 0;
+            this.update();
+        }
+        this.draw();
+
+        this.lastRender = timestamp;
+    }
 }
